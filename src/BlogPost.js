d2d4d13d90cbd7fd1d53c04ea076ff3d1e04ad38
Charlotte Warfel <charlottewarfel@gmail.com>
2019-02-20 08:38:50 -0800

    hover and active styling for nav bar

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import "./BlogPost.css";
 
 export class BlogPost extends Component {
   render() {
