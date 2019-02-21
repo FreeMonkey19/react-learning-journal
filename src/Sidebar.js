b1f15e06a120decc5652e6137d0dd21d4809bbb2
Charlotte Warfel <charlottewarfel@gmail.com>
2019-02-21 11:30:37 -0800

    style sidebar and its divs in preparation for images-add classNames to target approprately

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -6,29 +6,36 @@ export class Sidebar extends Component {
   render() {
     return (
       <div className="sidebar-container">
-        <div className="sidebar-content">
-          <div>
-            <h2 className="sidebar-quote">Sidebar Quote</h2>
+        <div>
+          <h2 className="sidebar-quote">Sidebar Quote</h2>
+        </div>
+        <div className="sidebar-nav">
+          <div className="sidebar-nav-link-container">
+            <Link
+              target="blank"
+              className="sidebar-nav-link"
+              to="https://github.com/charlottewarfel"
+            >
+              gitHub
+            </Link>
           </div>
-          <div className="sidebar-nav">
-            <div>
-              <Link
-                target="blank"
-                className="sidebar-nav-link"
-                to="https://github.com/charlottewarfel"
-              >
-                gitHub
-              </Link>
-            </div>
-            <div>
-              <Link
-                target="blank"
-                className="sidebar-nav-link"
-                to="https://www.linkedin.com/in/charlotte-warfel-481120177/"
-              >
-                LinkedIn
-              </Link>
-            </div>
+          <div className="sidebar-nav-link-container">
+            <Link
+              target="blank"
+              className="sidebar-nav-link"
+              to="https://www.linkedin.com/in/charlotte-warfel-481120177/"
+            >
+              LinkedIn
+            </Link>
+          </div>
+        </div>
+
+        <div className="sidebar-pics-container">
+          <div className="sidebar-pic">
+            Picture here hard coded for styling purposes
+          </div>
+          <div className="sidebar-pic">
+            Another picture hardcoded for styling purposes
           </div>
         </div>
       </div>
