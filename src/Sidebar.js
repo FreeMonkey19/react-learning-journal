178fd5bec7f238318c049d576140c485df0f86bb
Charlotte Warfel <charlottewarfel@gmail.com>
2019-02-25 06:10:29 -0800

    change styling to minimilistic view, remove placeholders and unused divs, add social media icons

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,55 +1,45 @@
 import React, { Component } from "react";
 import "./Sidebar.css";
-import rollerCoaster from "./images/roller-coaster.png";
-import staggerPic from "./images/stagger.png";
+import charlottePic from "./images/charlotte-work.PNG";
+import linkedin from "./images/linkedin.png";
+import octocat from "./images/octo-cat.png";
 
 export class Sidebar extends Component {
   render() {
     return (
       <div className="sidebar-container">
-        <div className="sidebar-quote-container">
-          <p className="sidebar-quote">
-            It's a wonderful day in the neighborhood, a wonderful day in the
-            neighborhood...a wonderful day!
-          </p>
-        </div>
-
-        <div className="sidebar-pics-container">
-          <div className="sidebar-pic">
-            <img
-              src={staggerPic}
-              alt="Cartoon roller coaster"
-              height="150"
-              width="200"
-            />
+        <div className="sidebar-wrapper">
+          <div className="sidebar-quote-container">
+            <p className="sidebar-quote">
+              It's a wonderful day in the neighborhood, a wonderful day in the
+              neighborhood...a wonderful day!
+            </p>
           </div>
-          <div className="sidebar-pic">
-            <img
-              src={rollerCoaster}
-              alt="Cartoon roller coaster"
-              height="150"
-              width="200"
-            />
-          </div>
-        </div>
-        <div className="sidebar-nav">
-          <a
-            className="sidebar-a-link"
-            href="https://github.com/charlottewarfel"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            gitHub
-          </a>
 
-          <a
-            className="sidebar-a-link"
-            href="https://www.linkedin.com/in/charlotte-warfel-481120177/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            LinkedIn
-          </a>
+          <div className="sidebar-pics-container">
+            <div className="sidebar-pic">
+              <img src={charlottePic} alt="Picture of author" />
+            </div>
+          </div>
+          <div className="sidebar-nav-container">
+            <a
+              className="sidebar-a-link"
+              href="https://github.com/charlottewarfel"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={octocat} alt="gitHub icon" height="28px" width="28px" />
+            </a>
+
+            <a
+              className="sidebar-a-link"
+              href="https://www.linkedin.com/in/charlotte-warfel-481120177/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={linkedin} alt="linkedin icon" />
+            </a>
+          </div>
         </div>
       </div>
     );
