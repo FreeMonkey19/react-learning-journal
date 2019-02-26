71e428d4cc659d75ad589ade7fcfed4a9cd89fbd
Charlotte Warfel <charlottewarfel@gmail.com>
2019-02-26 11:36:51 -0800

    add necessary input fields to create new post, add handleSubmit and handleChange functions, verify updated state in console

--- src/NewPost.js
+++ src/NewPost.js
@@ -1,13 +1,70 @@
 import React, { Component } from "react";
 
 export class NewPost extends Component {
+  state = {
+    title: "",
+    author: "",
+    createdOn: "",
+    body: "",
+    tags: []
+  };
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+  };
+
   render() {
     return (
-      <div className="form-container">
-        <form>
-          <button>Submit</button>
-        </form>
-      </div>
+      <form>
+        <input
+          type="text"
+          name="title"
+          placeholder="blog title"
+          value={this.state.title}
+          onChange={e => this.handleChange(e)}
+        />
+        <br />
+        <input
+          type="text"
+          name="author"
+          placeholder="author"
+          value={this.state.author}
+          onChange={e => this.handleChange(e)}
+        />
+        <br />
+        <input
+          type="date"
+          name="createdOn"
+          placeholder="createdOn"
+          value={this.state.createdOn}
+          onChange={e => this.handleChange(e)}
+        />
+        <br />
+        <input
+          type="textarea"
+          name="body"
+          placeholder="body"
+          value={this.state.body}
+          onChange={e => this.handleChange(e)}
+        />
+        <br />
+        <input
+          type="text"
+          placeholder="tags"
+          value={this.state.tags}
+          onChange={e => this.setState({ tags: e.target.value })}
+        />
+        <br />
+        <button onClick={e => this.handleSubmit(e)} type="submit">
+          Submit
+        </button>
+      </form>
     );
   }
 }
