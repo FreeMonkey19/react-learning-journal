8ef7ff3fddc33fa7b069ef39c652f1667d055456
Charlotte Warfel <charlottewarfel@gmail.com>
2019-03-20 14:27:07 -0700

    set up fake storage for testing

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,30 +3,6 @@
 import "react-testing-library/cleanup-after-each";
 // this adds jest-dom's custom assertions
 import "jest-dom/extend-expect";
-import data from "./data";
+import { data } from "./data";
 
-class LocalStorageMock {
-  constructor() {
-    this.store = {
-      posts: data
-    };
-  }
-
-  clear() {
-    this.store = {};
-  }
-
-  getItem(key) {
-    return this.store[key] || null;
-  }
-
-  setItem(key, value) {
-    this.store[key] = JSON.parse(localStorage.getItem(value));
-  }
-
-  removeItem(key) {
-    delete this.store[key];
-  }
-}
-
-global.localStorage = new LocalStorageMock();
+Storage.prototype.getItem = jest.fn(() => JSON.stringify(data));
