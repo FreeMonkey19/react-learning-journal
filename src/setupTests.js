77a998da042dbacb92ce5bf84a9e447cc7a93daf
Charlotte Warfel <charlottewarfel@gmail.com>
2019-03-20 13:47:57 -0700

    add LocalStorageMock to setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,30 @@
 import "react-testing-library/cleanup-after-each";
 // this adds jest-dom's custom assertions
 import "jest-dom/extend-expect";
+import data from "./data";
+
+class LocalStorageMock {
+  constructor() {
+    this.store = {
+      posts: data
+    };
+  }
+
+  clear() {
+    this.store = {};
+  }
+
+  getItem(key) {
+    return this.store[key] || null;
+  }
+
+  setItem(key, value) {
+    this.store[key] = JSON.parse(localStorage.getItem(value));
+  }
+
+  removeItem(key) {
+    delete this.store[key];
+  }
+}
+
+global.localStorage = new LocalStorageMock();
